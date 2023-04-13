625eed417a09d715832ee3fcd0c1704ba3b0242f
1okmon <alexma88868@live.com>
2023-04-13 12:45:20 +0700

    Создал enum для ситуаций когда машин для отображения нет

---
+++ homework-1/homework-1/Enums/CarsNotFound.swift
@@ -0,0 +1,13 @@
+//
+//  CarsNotFound.swift
+//  homework-1
+//
+//  Created by 1okmon on 13.04.2023.
+//
+
+import Foundation
+
+enum CarsNotFound: String {
+    case noCarAtAll = "Автомобили не найдейны!"
+    case noCarWithBodyFilter = "Автомобили с данным типом кузова не найдейны!"
+}
