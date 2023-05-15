358ccf704c2f20faea4d7761ccaefea7fdbcebd8
1okmon <alexma88868@live.com>
2023-05-15 11:56:24 +0700

    поправил контроллер для коллекции

--- homework-5 MVVM/homework-5 MVVM/Sources/CarsCollection/View/CarsViewController.swift
+++ homework-5 MVVM/homework-5 MVVM/Sources/CarsCollection/View/CarsViewController.swift
@@ -7,88 +7,24 @@
 
 import UIKit
 
-fileprivate enum Cars: CaseIterable {
-    case skylineR34
-    case skylineR35
-    case supraA80
-    case supraA90
-    case silviaS13
-    case silviaS14
-    case silviaS15
-    case silviaS15Boss
-    case z370BadAssVRZ
-    case miata
-    
-    var carModel: CarModel {
-        switch self {
-        case .skylineR34:
-            return CarModel(manufacturer: "Nissan",
-                            model: "GT-R r34",
-                            images: [UIImage(named: "skylineR34-1"),
-                                     UIImage(named: "skylineR34-2"),
-                                     UIImage(named: "skylineR34-3")])
-        case .skylineR35:
-            return CarModel(manufacturer: "Nissan",
-                            model: "GT-R",
-                            images: [UIImage(named: "skylineR35-1"),
-                                     UIImage(named: "skylineR35-2")])
-        case .supraA80:
-            return CarModel(manufacturer: "Toyota",
-                            model: "Supra A80",
-                            images: [UIImage(named: "supraA80-1")])
-        case .supraA90:
-            return CarModel(manufacturer: "Toyota",
-                            model: "Supra A90",
-                            images: [UIImage(named: "supraA90-1")])
-        case .silviaS13:
-            return CarModel(manufacturer: "Nissan",
-                            model: "Silvia S13",
-                            images: [UIImage(named: "silviaS13-1"),
-                                     UIImage(named: "silviaS13-2")])
-        case .silviaS14:
-            return CarModel(manufacturer: "Nissan",
-                            model: "Silvia S14",
-                            images: [UIImage(named: "silviaS14-1")])
-        case .silviaS15:
-            return CarModel(manufacturer: "Nissan",
-                            model: "Silvia S15",
-                            images: [UIImage(named: "silviaS15-1"),
-                                     UIImage(named: "silviaS15-2")])
-        case .silviaS15Boss:
-            return CarModel(manufacturer: "Nissan",
-                            model: "Silvia S15 rocket bunny boss",
-                            images: [UIImage(named: "silviaS15Boss-1"),
-                                     UIImage(named: "silviaS15Boss-2")])
-        case .z370BadAssVRZ:
-            return CarModel(manufacturer: "Nissan",
-                            model: "370Z BadAss VR-Z",
-                            images: [UIImage(named: "z370BadAssVRZ-1"),
-                                     UIImage(named: "z370BadAssVRZ-2")])
-        case .miata:
-            return CarModel(manufacturer: "Mazda",
-                            model: "MX-5 miata",
-                            images: [UIImage(named: "miata-1"),
-                                     UIImage(named: "miata-2"),
-                                     UIImage(named: "miata-3")])
-        }
-    }
-    
-    static var allCarModels: [CarModel] {
-        var carModels = [CarModel]()
-        for car in Cars.allCases {
-            carModels.append(car.carModel)
-        }
-        return carModels
-    }
-}
-
-fileprivate enum ViewControllerMetrics {
-    static let title = "Cars list"
+private enum Metrics {
+    static let viewTitle = "Cars list"
     static let viewBackgroundColor = UIColor.white
 }
 
 final class CarsViewController: UIViewController {
-    private var configureLayout: (()->()) = {}
+    private var viewModel: ICarsViewModel
+    
+    init(viewModel: ICarsViewModel) {
+        self.viewModel = viewModel
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    private var configureLayout: (()->Void) = {}
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -103,8 +39,8 @@ final class CarsViewController: UIViewController {
 
 private extension CarsViewController {
     func configure() {
-        let carsView = CarsView(carModels: Cars.allCarModels)
-        title = ViewControllerMetrics.title
+        let carsView = CarsView(carsViewModel: viewModel)
+        title = Metrics.viewTitle
         configure(view: view)
         configure(carsView: carsView)
     }
@@ -124,6 +60,6 @@ private extension CarsViewController {
     }
     
     func configure(view: UIView) {
-        view.backgroundColor = ViewControllerMetrics.viewBackgroundColor
+        view.backgroundColor = Metrics.viewBackgroundColor
     }
 }
