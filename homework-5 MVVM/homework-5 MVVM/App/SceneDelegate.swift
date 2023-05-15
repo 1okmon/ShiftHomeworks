51dc0fcb95bc6b7451c6197843e2dabc2d1164ac
1okmon <alexma88868@live.com>
2023-05-15 11:58:59 +0700

    добавил координатора

--- homework-5 MVVM/homework-5 MVVM/App/SceneDelegate.swift
+++ homework-5 MVVM/homework-5 MVVM/App/SceneDelegate.swift
@@ -10,13 +10,15 @@ import UIKit
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     var window: UIWindow?
-
+    var appCoordinator : AppCoordinator?
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         guard let windowScene = (scene as? UIWindowScene) else { return }
         window = UIWindow(windowScene: windowScene)
-        let navigationViewController = NavigationController()
-        window?.rootViewController = navigationViewController
+        let navigationController = UINavigationController.init()
+        appCoordinator = AppCoordinator(navigationController: navigationController)
+        appCoordinator?.start()
+        window?.rootViewController = navigationController
         window?.makeKeyAndVisible()
     }
 
---
+++ homework-5 MVVM/homework-5 MVVM/Sources/AppCoordinator.swift
@@ -0,0 +1,43 @@
+//
+//  AppCoordinator.swift
+//  CollectionApp
+//
+//  Created by 1okmon on 12.05.2023.
+//
+
+import UIKit
+
+class AppCoordinator {
+    private var navigationController: UINavigationController
+    
+    init(navigationController: UINavigationController) {
+        self.navigationController = navigationController
+    }
+    
+    func start() {
+        let viewModel = CarsViewModel()
+        viewModel.coordinator = self
+        let carsViewController = CarsViewController(viewModel: viewModel)
+        navigationController.viewControllers = [carsViewController]
+    }
+    
+    func goToCarDetails(with car: Car) {
+        let viewModel = CarDetailsViewModel(car: car)
+        viewModel.coordinator = self
+        let carDetailsViewController = CarDetailsViewController(viewModel: viewModel)
+        navigationController.pushViewController(carDetailsViewController, animated: true)
+    }
+    
+    func goToCarPhotoCarousel(with car: Car) {
+        let viewModel = CarPhotoCarouselViewModel(car: car)
+        viewModel.coordinator = self
+        let carPhotoCarouselViewController = CarPhotoCarouselViewController(viewModel: viewModel)
+        let navController = UINavigationController(rootViewController: carPhotoCarouselViewController)
+        navController.modalPresentationStyle = .fullScreen
+        navigationController.present(navController, animated: true)
+    }
+    
+    func dismiss() {
+        navigationController.dismiss(animated: true)
+    }
+}
