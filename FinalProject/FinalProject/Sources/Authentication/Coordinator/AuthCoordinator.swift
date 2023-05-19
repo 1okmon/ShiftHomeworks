baf3324326fb0e920e14ee6ea315731dd37ac047
1okmon <alexma88868@live.com>
2023-05-19 10:41:23 +0700

    Создал вью модель и координатор

---
+++ FinalProject/FinalProject/Sources/Authentication/Coordinator/AuthCoordinator.swift
@@ -0,0 +1,28 @@
+//
+//  Coordinator.swift
+//  FinalProject
+//
+//  Created by 1okmon on 18.05.2023.
+//
+
+import UIKit
+
+final class AuthCoordinator: IAuthCoordinator {
+    private var navigationController: UINavigationController
+    
+    init(navigationController: UINavigationController) {
+        self.navigationController = navigationController
+    }
+    
+    func start() {
+    }
+    
+    func goToSignIn() {
+    }
+    
+    func goToSignUp() {
+    }
+    
+    func goToResetPassword() {
+    }
+}
