160066afe819065f32176fef7bfc7813de1c1c52
1okmon <alexma88868@live.com>
2023-05-19 10:47:33 +0700

    Реализовал паттерн декоратор для типа кнопки используемой при входе в приложение

---
+++ FinalProject/FinalProject/Sources/Authentication/View/AuthButton/ButtonTypes/SignUpButton.swift
@@ -0,0 +1,13 @@
+//
+//  SignUpButton.swift
+//  FinalProject
+//
+//  Created by 1okmon on 18.05.2023.
+//
+
+final class SignUpButton: AuthButtonDecorator {
+    override func didTapped() {
+        super.didTapped()
+        print("sigUp")
+    }
+}
---
+++ FinalProject/FinalProject/Sources/Authentication/View/AuthButton/IAuthButton.swift
@@ -0,0 +1,12 @@
+//
+//  IAuthButton.swift
+//  FinalProject
+//
+//  Created by 1okmon on 18.05.2023.
+//
+
+import UIKit
+
+protocol IAuthButton: UIButton {
+    func didTapped()
+}
