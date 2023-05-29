da03b52c7ca7ad3b25705147b3c2ed65de97f137
1okmon <alexma88868@live.com>
2023-05-29 11:05:30 +0700

    Реализовал общение с firebase

---
+++ FinalProject/FinalProject/Sources/Authentication/Auth/Additions/FirebaseNetworkManager/ErrorParser/ErrorParser.swift
@@ -0,0 +1,36 @@
+//
+//  ErrorParser.swift
+//  FinalProject
+//
+//  Created by 1okmon on 26.05.2023.
+//
+
+import Firebase
+final class ErrorParser {
+    func parse(error: Error) -> AuthResult {
+        if let errCode = AuthErrorCode.Code(rawValue: error._code) {
+            print("in")
+            switch errCode {
+            case .wrongPassword:
+                return .wrongPassword
+            case .userNotFound:
+                return .userNotFound
+            case .weakPassword:
+                return .weakPassword
+            case .emailAlreadyInUse:
+                return .emailAlreadyInUse
+            case .networkError:
+                return .networkError
+            case .internalError:
+                return .serverError
+            case .tooManyRequests:
+                return .tooManyRequests
+            case .invalidEmail:
+                return .badEmailFormat
+            default:
+                return .undefinedError
+            }
+        }
+        return .undefinedError
+    }
+}
