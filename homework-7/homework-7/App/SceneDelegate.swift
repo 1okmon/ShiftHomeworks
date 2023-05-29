c6242b9d8ac34b42c471203c3814fa59829633e2
1okmon <alexma88868@live.com>
2023-05-29 12:15:32 +0700

    Создал проект и добавил под(swiftLint)

---
+++ homework-7/homework-7/App/SceneDelegate.swift
@@ -0,0 +1,17 @@
+//
+//  SceneDelegate.swift
+//  homework-7
+//
+//  Created by 1okmon on 29.05.2023.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+    var window: UIWindow?
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        window = UIWindow(windowScene: windowScene)
+    }
+}
