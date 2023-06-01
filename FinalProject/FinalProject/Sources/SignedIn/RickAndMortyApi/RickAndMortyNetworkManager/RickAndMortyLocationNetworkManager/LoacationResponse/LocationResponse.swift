7b15335169d2c0e108223ad8d1f678033f805c5a
1okmon <alexma88868@live.com>
2023-06-01 09:03:23 +0700

    Реализовал менеджера для загрузки локаций

---
+++ FinalProject/FinalProject/Sources/SignedIn/RickAndMortyApi/RickAndMortyNetworkManager/RickAndMortyLocationNetworkManager/LoacationResponse/LocationResponse.swift
@@ -0,0 +1,16 @@
+//
+//  LocationResponse.swift
+//  FinalProject
+//
+//  Created by 1okmon on 30.05.2023.
+//
+
+import Foundation
+
+struct LocationResponse: Decodable {
+    let id: Int
+    let name: String
+    let type: String
+    let dimension: String
+    let residents: [String]
+}
