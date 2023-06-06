baae3e362bb2ae3afa74e4457cbe030d2955176c
1okmon <alexma88868@live.com>
2023-06-06 15:58:37 +0700

    вынес отдельно изображение для кнопки избранное

---
+++ FinalProject/FinalProject/Sources/Design/Icon.swift
@@ -0,0 +1,22 @@
+//
+//  Icon.swift
+//  FinalProject
+//
+//  Created by 1okmon on 06.06.2023.
+//
+
+import UIKit
+
+enum Icon {
+    enum Favorite {
+        private static let added = UIImage(systemName: "star.fill")
+        private static let notAdded = UIImage(systemName: "star")
+        
+        static func image(_ isFavorite: Bool = false) -> UIImage? {
+            if isFavorite {
+                return added
+            }
+            return notAdded
+        }
+    }
+}
