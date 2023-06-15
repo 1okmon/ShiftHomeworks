68631dc60294667e8e43b054fe2b82d2cf9484db
1okmon <alexma88868@live.com>
2023-06-15 10:10:00 +0700

    правки в цветах темы

--- FinalProject/FinalProject/Sources/Design/Theme.swift
+++ FinalProject/FinalProject/Sources/Design/Theme.swift
@@ -9,12 +9,12 @@ import UIKit
 
 enum Theme {
     static let backgroundColor = UIColor.color(light: .white, dark: .black)
-    static let collectionViewBackgroundColor = UIColor.color(light: .lightGray, dark: .gray)
+    static let collectionViewBackgroundColor = UIColor.color(light: .lightGray, dark: .systemGray6)
     static let textColor = UIColor.color(light: .black, dark: .white)
     static let tintColor = UIColor.color(light: .black, dark: .white)
     static let itemsBackgroundColor = UIColor.color(light: .white, dark: .darkGray)
     static let placeholderColor = UIColor.color(light: .gray, dark: .lightGray)
-    static let tableViewSeparatorColor = UIColor.color(light: .gray, dark: .lightGray)
+    static let separatorColor = UIColor.color(light: .gray, dark: .lightGray)
     static var borderCgColor: CGColor { UIColor.color(light: .black, dark: .lightGray).cgColor }
 }
 
